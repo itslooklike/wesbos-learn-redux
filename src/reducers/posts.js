69324985a97bfabab99adbc41aeab104978e1bc6
itslooklike <itslooklike@users.noreply.github.com>
2017-11-29 14:27:03 +0300

    add Raven

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -7,7 +7,7 @@ const posts = (state = initialState, { type, index }) => {
   switch (type) {
     case "INCREMENT_LIKES":
       const post = state.findIndex(item => item.code === index);
-      
+
       return updateIn(state, [post, "likes"], val => val + 1);
     default:
       return state;
